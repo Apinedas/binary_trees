93fb5fbe06c1110053bef5959e268efc855d71c8
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-03-22 09:45:31 +0000

    fix: Change on 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,7 +17,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!new)
 		return (NULL);
 	if (parent->left)
+	{
 		new->left = parent->left;
+		new->left->parent = new;
+	}
 	parent->left = new;
 	return (new);
 }
