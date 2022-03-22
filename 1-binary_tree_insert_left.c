6b0b3e27bd5dd63bdedcfe6f2015129c0c59d568
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-03-22 08:57:05 +0000

    feat: added 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: pointer to the node to insert the left-child in
+ * @value: is the value to store in the new node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
+	if (parent->left)
+		new->left = parent->left;
+	parent->left = new;
+	return (new);
+}
