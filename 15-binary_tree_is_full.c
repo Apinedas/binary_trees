fc66bcb00791d59e7d270d3cfabb2bd2991265a3
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-03-22 13:37:45 +0000

    feat: Added 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if tree is full, 0 otherwise
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree || (!(tree->left) && tree->right) || (!(tree->right) && tree->left))
+		return (0);
+	if (!(tree->left) && !(tree->right))
+		return (1);
+	return (binary_tree_is_full(tree->left) * binary_tree_is_full(tree->right));
+}
